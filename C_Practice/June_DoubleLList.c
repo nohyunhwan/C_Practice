c3ac7602cdc1020e223f496166be4dc9f3b97f5e
JuneKim <rlawns3244@naver.com>
2019-02-02 12:35:17 +0900

    DoublyLinkedList_0202
    Average Optimization

--- C_Practice/June_DoubleLList.c
+++ C_Practice/June_DoubleLList.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*
+과제: 더블링크드리스트 탐색 최적화
+평균값을 구해서, Head부터 탐색할지, Tail부터 탐색할지 판단하기
+//1.현재 리스트의 평균을 구한 후,
+2.삽입하려는 값이 평균보다 작거나 같으면, 헤드부터 탐색 후 삽입 //Comp Func CallBack??
+3.삽입하려는 값이 평균보다 크면, 테일부터 탐색 후 삽입
+*/
 
 typedef struct _node {
 	int nData;
@@ -8,8 +15,65 @@ typedef struct _node {
 }NODE;
 NODE* head = NULL;
 NODE* tail = NULL;
+int avg = 0, count = 1;
 
-void InsertNode(int _nData) {
+int GetAverage() {
+	int sum = 0;
+	NODE* pNode = head;
+	while (NULL != pNode) {
+		sum += pNode->nData;
+		pNode = pNode->pNext;
+		count++;
+	}
+	avg = sum / count;
+	return avg;
+}
+
+int FindNode(int _nData) {
+
+}
+
+void InsertNodeFromTail(int _nData) {
+	NODE* pNewNode = (NODE*)malloc(sizeof(NODE));
+	pNewNode->pNext = NULL;
+	pNewNode->pPrev = NULL;
+	pNewNode->nData = _nData;
+
+	if (NULL == tail) {
+		head = pNewNode;
+		tail = pNewNode;
+	}
+	else {
+		NODE* pNode = tail;
+		//Case by Case
+		if (pNewNode->nData >= pNode->nData) {
+			pNewNode->pNext = NULL;
+			pNewNode->pPrev = pNode;
+			pNode->pNext = pNewNode;
+			tail = pNewNode;
+		}
+		else {
+			//Mid
+			while (NULL != pNode->pPrev) {
+				if (pNewNode->nData >= pNode->pPrev->nData) {
+					pNewNode->pNext = pNode;
+					pNewNode->pPrev = pNode->pPrev;
+					pNode->pPrev->pNext = pNewNode;
+					pNode->pPrev = pNewNode;
+					break;
+				}
+				pNode = pNode->pPrev;
+			}
+			if (pNode->pPrev == NULL) {
+				pNewNode->pNext = pNode;
+				pNode->pPrev = pNewNode;
+				head = pNewNode;
+			}
+		}
+	}
+}
+
+void InsertNodeFromHead(int _nData) {
 	NODE* pNewNode = (NODE*)malloc(sizeof(NODE));
 	pNewNode->pNext = NULL;
 	pNewNode->pPrev = NULL;
@@ -17,6 +81,7 @@ void InsertNode(int _nData) {
 
 	if (NULL == head) {
 		head = pNewNode;
+		tail = pNewNode;
 	}
 	else {
 		NODE* pNode = head;
@@ -41,6 +106,7 @@ void InsertNode(int _nData) {
 			if (pNode->pNext == NULL) {
 				pNewNode->pPrev = pNode;
 				pNode->pNext = pNewNode;
+				tail = pNewNode;
 			}
 		}
 	}
@@ -65,11 +131,21 @@ void PrintList_Tail() {
 }
 
 int main(void) {
-	InsertNode(1);
-	InsertNode(10);
-	InsertNode(7);
-	InsertNode(4);
-	InsertNode(5);
+	int input = 0;
+	while (1) {
+		printf("Input Data : ");
+		scanf_s("%d", &input);
+		if (input == EOF) break; //-1, Break
+
+		if (input >= GetAverage()) {
+			printf("\n Average = %d\n", GetAverage());
+			InsertNodeFromHead(input);
+		}
+		else {
+			printf("\n Average = %d\n", GetAverage());
+			InsertNodeFromTail(input);
+		}
+	}
 	PrintList_Head();
 	PrintList_Tail();
 	return 0;
